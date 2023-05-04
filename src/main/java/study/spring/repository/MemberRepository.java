7fcb45ae3d5ed27592b536b51c3a501f84e71825
tneserp <tneserp2@gmail.com>
2023-05-04 12:13:34 +0900

    refactor: 회원가입 기능 Spring data JPA을 이용하도록 리팩토링

--- src/main/java/study/spring/repository/MemberRepository.java
+++ src/main/java/study/spring/repository/MemberRepository.java
@@ -1,17 +1,10 @@
 package study.spring.repository;
 
-import javax.persistence.EntityManager;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import study.spring.domain.Member;
 
 @Repository
-@RequiredArgsConstructor
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    private final EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
 }
